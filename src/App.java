edcb805edad334264aa5fd4d05d4591e16cedb80
Rodrigo Nathan Oliveira <rodrigonathan4101@gmail.com>
2021-10-27 23:35:46 -0300

    Código corrigido e aprimorado

--- src/App.java
+++ src/App.java
@@ -9,10 +9,23 @@ public class App {
         if (opcao <1 || opcao >3){
             JOptionPane.showMessageDialog(null, "Opção invalida");
         }
-        else{        
-            JOptionPane.showMessageDialog(null, "Você selecionou: " + opcao, "Peça selecionada", JOptionPane.INFORMATION_MESSAGE);
+        else{                    
+            if (opcao == 1){
+            JOptionPane.showMessageDialog(null, "Você escolheu pedra", "Opção selecionada", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (opcao == 2){
+                JOptionPane.showMessageDialog(null, "Você escolheu papel", "Opção selecionada", JOptionPane.INFORMATION_MESSAGE);
+            }
+            if (opcao == 3){
+                JOptionPane.showMessageDialog(null, "Você escolheu tesoura", "Opção selecionada", JOptionPane.INFORMATION_MESSAGE);
+            }
+            
+
             Random gerador = new Random();
             int numero = gerador.nextInt(3) + 1;
+          
+            
+
             if (numero == 1){
             JOptionPane.showMessageDialog(null, "A maquina escolheu pedra");
             }
@@ -23,7 +36,13 @@ public class App {
                 JOptionPane.showMessageDialog(null, "A maquina escolheu tesoura");
             }
 
+    
             int resultado = opcao - numero;
+                if (opcao ==1 && numero ==3 ){
+                  JOptionPane.showMessageDialog(null, "Você ganhou!");
+                return;
+
+                }
                 if (resultado == 0){
                     JOptionPane.showMessageDialog(null, "Jogo empatado!");
                 }
